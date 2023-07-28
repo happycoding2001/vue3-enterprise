4303bdae78292ce82bd68c28f70545aed6d45039
dingming <ddd>
2023-07-28 15:43:54 +0800

    defineProps 和 defineEmits

---
+++ src/components/Test.vue
@@ -0,0 +1,14 @@
+<template>
+    <div @click="add">{{ props.count }}</div>
+  </template>
+  
+  <script setup>
+  const props = defineProps({ count: Number })
+  console.log('props', props)
+  const emit = defineEmits(['add'])
+
+  const add = ()=>{
+    emit('add')
+  }
+  </script>
+  
