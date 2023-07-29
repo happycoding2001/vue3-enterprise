cdac4ceef4ac5ff35ce73051f63850ebc3c3b897
dingming <ddd>
2023-07-29 16:18:34 +0800

    save

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,26 @@
 <!--App.vue-->
 <template>
-  <div>
+<div>
     <h1>提供/注入</h1>
     <Father />
-  </div>
-
+</div>
 </template>
 
-<script setup>
-import { provide } from 'vue'
+<script>
+import {
+    provide
+} from 'vue'
 import Father from './components/Father.vue'
 
 provide('name', '陈尼克') // 单个声明形式
 provide('info', {
-  work: '前端开发',
-  age: '18'
+    work: '前端开发',
+    age: '18'
 }) // 多个声明形式
+
+const changeName = () => {
+    name.value = '李尼克'
+}
+
+provide('changeName', changeName)
 </script>
--- src/components/Son.vue
+++ src/components/Son.vue
@@ -3,11 +3,13 @@
     <div>我是儿子，{{ name }}</div>
     <div>职业：{{ info.work }}</div>
     <div>年龄：{{ info.age }}</div>
+    <button @click="changeName">修改名字</button>
   </template>
   
   <script setup>
   import { inject } from 'vue'
   const name = inject('name', '嘻嘻') // 第二个参数为默认值，可选
   const info = inject('info')
+  const changeName =inject('changeName')
   </script>
   
