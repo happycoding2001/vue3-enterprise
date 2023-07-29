4bf4106b672520095eb45d3fc66f90ff08eae8b7
dingming <ddd>
2023-07-29 22:43:12 +0800

    局部引入

--- src/main.js
+++ src/main.js
@@ -1,10 +1,16 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import ElementPlus from 'element-plus'
 import router from '@/router'
+
+// import 'element-plus/lib/theme-chalk/index.css';  会报错
+import 'element-plus/dist/index.css';
 
 // 生成 app 实例。
 const app = createApp(App)
 // 通过 use 引入 路由实例。
 app.use(router)
 // 将实例挂载到 #app 节点上。
+// app.use(ElementPlus)
+app.use(ElButton)
 app.mount('#app')
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,7 @@
 <template>
 <router-link :to="{path:'/about',query:{id:1}}">Home</router-link>
 <div @click="linkTo">跳转{{ENV}}</div>
+<el-button type="primary">主要按钮</el-button>
 </template>
 
 <script setup>
