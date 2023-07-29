8e702e085f0a549eb71353132c23c9da4c9f0084
dingming <ddd>
2023-07-29 23:09:49 +0800

    save

--- src/main.js
+++ src/main.js
@@ -4,13 +4,13 @@ import ElementPlus from 'element-plus'
 import router from '@/router'
 
 // import 'element-plus/lib/theme-chalk/index.css';  会报错
-import 'element-plus/dist/index.css';
+import 'element-plus/dist/index.css';  
 
 // 生成 app 实例。
 const app = createApp(App)
 // 通过 use 引入 路由实例。
 app.use(router)
 // 将实例挂载到 #app 节点上。
-// app.use(ElementPlus)
-app.use(ElButton)
+app.use(ElementPlus)
+// app.use(ElButton)
 app.mount('#app')
--- vite.config.js
+++ vite.config.js
@@ -1,30 +1,31 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
-import vitePluginImport from 'vite-plugin-babel-import'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import ElementPlus from 'unplugin-element-plus/vite' // 不加这个配置，ElMessage出不来
 
 // https://vitejs.dev/config/
 export default ({ mode }) => defineConfig({
   plugins: [
     vue(),
-    vitePluginImport([
-      {
-        libraryName: 'element-plus',
-        libraryDirectory: 'es',
-        style(name) {
-          return `element-plus/lib/theme-chalk/${name}.css`;
-        },
-      }
-    ]),
+    // 按需引入，主题色的配置，需要加上 importStyle: 'sass'
+    Components({
+      resolvers: [ElementPlusResolver({
+        importStyle: 'sass'
+      })],
+    }),
+    // 用于内部方法调用，样式缺失的现象，如 ElMessage 等
+    ElementPlus({ useSource: true })
   ],
   resolve: {
     alias: {
       '~': path.resolve(__dirname, './'),
       '@': path.resolve(__dirname, 'src')
     },
-    extensions: ['.vue', '.js', 'jsx', '.json']
   },
-  base: mode == 'development' ? './' : (mode == 'beta' ? '//s.baidu.com/beta/xxx' : '//s.baidu.com/release/xxx'), // 静态资源路径配置
+  base: './',
   server: {
     proxy: {
       '/api': {
@@ -33,5 +34,13 @@ export default ({ mode }) => defineConfig({
         rewrite: path => path.replace(/^\/api/, '') // 重写 api 为 空，就是去掉它
       }
     }
+  },
+  css: {
+    preprocessorOptions: {
+      // 覆盖掉element-plus包中的主题变量文件
+      scss: {
+        additionalData: `@use "@/styles/element/index.scss" as *;`,
+      },
+    },
   },
 })
