39deae9415ff45f7feff6e1f2096a52f3dd54888
dingming <ddd>
2023-07-29 22:37:12 +0800

    save

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,17 @@
 <!--Home.vue-->
 <template>
 <router-link :to="{path:'/about',query:{id:1}}">Home</router-link>
-<div @click="linkTo">跳转</div>
+<div @click="linkTo">跳转{{ENV}}</div>
 </template>
 
 <script setup>
+const ENV = import.meta.env
 import {
     useRouter
 } from 'vue-router'
 const router = useRouter()
 const linkTo = () => {
-    
+
     router.push('/about')
 }
 
