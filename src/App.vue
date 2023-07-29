7693a3ea78067cf8b10bd06542106a0edb03baa7
dingming <ddd>
2023-07-29 16:15:40 +0800

    privide/inject

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,4 @@
+<!--App.vue-->
 <template>
   <div>
     <h1>提供/注入</h1>
@@ -6,15 +7,13 @@
 
 </template>
 
-<script>
+<script setup>
+import { provide } from 'vue'
 import Father from './components/Father.vue'
 
-export default {
-  components: {
-    Father
-  },
-  provide: {
-    name: '陈尼克'
-  }
-}
+provide('name', '陈尼克') // 单个声明形式
+provide('info', {
+  work: '前端开发',
+  age: '18'
+}) // 多个声明形式
 </script>
--- src/components/Son.vue
+++ src/components/Son.vue
@@ -1,12 +1,13 @@
 <!--Son.vue-->
 <template>
     <div>我是儿子，{{ name }}</div>
+    <div>职业：{{ info.work }}</div>
+    <div>年龄：{{ info.age }}</div>
   </template>
   
-  <script>
-  export default {
-    name: 'Son',
-    inject: ['name']
-  }
+  <script setup>
+  import { inject } from 'vue'
+  const name = inject('name', '嘻嘻') // 第二个参数为默认值，可选
+  const info = inject('info')
   </script>
   
