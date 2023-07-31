4b5755e5304d876faf03ad21e2819670970ba751
dingming <ddd>
2023-07-31 14:29:34 +0800

    公用方法封装

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,10 +4,18 @@
 <div @click="linkTo">跳转{{ENV}}</div>
  
 <el-button type="primary">主要按钮</el-button>
+<el-input v-model="val"></el-input>
+<el-button @click="save" type="primary">存</el-button>
+<el-button @click="load" type="primary">取</el-button>
 <el-icon><Setting /></el-icon>
+
 </template>
 
 <script setup>
+import {ref} from 'vue'
+import {Setting} from '@element-plus/icons-vue'
+
+import {localGet,localSet,localRemove} from '@/utils'
 const ENV = import.meta.env
 import {
     useRouter
@@ -17,5 +25,11 @@ const linkTo = () => {
 
     router.push('/about')
 }
-
+const val = ref(123)
+const save = ()=>{
+    localSet('tt','ssds'+val.value)
+}
+const load = ()=>{
+   val.value = localGet('tt')
+}
 </script>
