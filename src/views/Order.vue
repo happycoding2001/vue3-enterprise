9bb5d0e79f3b4eb8b9dba66ce2075c670829f30f
dingming <ddd>
2023-10-19 09:40:47 +0800

    save

--- src/views/Order.vue
+++ src/views/Order.vue
@@ -2,20 +2,10 @@
   <el-card class="order-container">
     <template #header>
       <div class="header">
-        <el-input
-          style="width: 200px; margin-right: 10px"
-          placeholder="请输入订单号"
-          v-model="state.orderNo"
-          @change="handleOption"
-          clearable
-        />
+        <el-input style="width: 200px; margin-right: 10px" placeholder="请输入订单号" v-model="state.orderNo"
+          @change="handleOption" clearable />
         <el-select @change="handleOption" v-model="state.orderStatus" style="width: 200px; margin-right: 10px">
-          <el-option
-            v-for="item in state.options"
-            :key="item.value"
-            :label="item.label"
-            :value="item.value"
-          />
+          <el-option v-for="item in state.options" :key="item.value" :label="item.label" :value="item.value" />
         </el-select>
         <!-- <el-button type="primary" size="small" icon="el-icon-edit">修改订单</el-button> -->
         <el-button type="primary" :icon="HomeFilled" @click="handleConfig()">配货完成</el-button>
@@ -23,104 +13,60 @@
         <el-button type="danger" :icon="Delete" @click="handleClose()">关闭订单</el-button>
       </div>
     </template>
-    <el-table
-      :load="state.loading"
-      :data="state.tableData"
-      tooltip-effect="dark"
-      style="width: 100%"
+    <el-table :load="state.loading" :data="state.tableData" tooltip-effect="dark" style="width: 100%"
       @selection-change="handleSelectionChange">
-      <el-table-column
-        type="selection"
-        width="55">
+      <el-table-column type="selection" width="55">
       </el-table-column>
-      <el-table-column
-        prop="orderNo"
-        label="订单号"
-      >
+      <el-table-column prop="orderNo" label="订单号">
       </el-table-column>
-      <el-table-column
-        prop="totalPrice"
-        label="订单总价"
-      >
+      <el-table-column prop="totalPrice" label="订单总价">
       </el-table-column>
-      <el-table-column
-        prop="orderStatus"
-        label="订单状态"
-      >
+      <el-table-column prop="orderStatus" label="订单状态">
         <template #default="scope">
           <span>{{ $filters.orderMap(scope.row.orderStatus) }}</span>
         </template>
       </el-table-column>
-      <el-table-column
-        prop="payType"
-        label="支付方式"
-      >
+      <el-table-column prop="payType" label="支付方式">
         <template #default='scope'>
           <span v-if="scope.row.payType == 1">微信支付</span>
           <span v-else-if="scope.row.payType == 2">支付宝支付</span>
           <span v-else>未知</span>
         </template>
       </el-table-column>
-      <el-table-column
-        prop="createTime"
-        label="创建时间"
-      >
+      <el-table-column prop="createTime" label="创建时间">
       </el-table-column>
-      <el-table-column
-        label="操作"
-      >
+      <el-table-column label="操作">
         <template #default="scope">
-          <el-popconfirm
-            v-if="scope.row.orderStatus == 1"
-            title="确定配货完成吗？"
-            @confirm="handleConfig(scope.row.orderId)"
-            confirm-button-text="确定"
-            cancel-button-text="取消"
-          >
+          <el-popconfirm v-if="scope.row.orderStatus == 1" title="确定配货完成吗？" @confirm="handleConfig(scope.row.orderId)"
+            confirm-button-text="确定" cancel-button-text="取消">
             <template #reference>
               <a style="cursor: pointer; margin-right: 10px">配货完成</a>
             </template>
           </el-popconfirm>
-          <el-popconfirm
-            v-if="scope.row.orderStatus == 2"
-            title="确定出库吗？"
-            @confirm="handleSend(scope.row.orderId)"
-            confirm-button-text="确定"
-            cancel-button-text="取消"
-          >
+          <el-popconfirm v-if="scope.row.orderStatus == 2" title="确定出库吗？" @confirm="handleSend(scope.row.orderId)"
+            confirm-button-text="确定" cancel-button-text="取消">
             <template #reference>
               <a style="cursor: pointer; margin-right: 10px">出库</a>
             </template>
           </el-popconfirm>
-          <el-popconfirm
-            v-if="!(scope.row.orderStatus == 4 || scope.row.orderStatus < 0)"
-            title="确定关闭订单吗？"
-            @confirm="handleClose(scope.row.orderId)"
-            confirm-button-text="确定"
-            cancel-button-text="取消"
-          >
+          <el-popconfirm v-if="!(scope.row.orderStatus == 4 || scope.row.orderStatus < 0)" title="确定关闭订单吗？"
+            @confirm="handleClose(scope.row.orderId)" confirm-button-text="确定" cancel-button-text="取消">
             <template #reference>
               <a style="cursor: pointer; margin-right: 10px">关闭订单</a>
             </template>
           </el-popconfirm>
-          <router-link :to="{ path: '/order_detail', query: { id: scope.row.orderId }}">订单详情</router-link>
+          <router-link :to="{ path: '/order_detail', query: { id: scope.row.orderId } }">订单详情</router-link>
         </template>
       </el-table-column>
     </el-table>
-    <el-pagination
-      background
-      layout="prev, pager, next"
-      :total="state.total"
-      :page-size="state.pageSize"
-      :current-page="state.currentPage"
-      @current-change="changePage"
-    />
+    <el-pagination background layout="prev, pager, next" :total="state.total" :page-size="state.pageSize"
+      :current-page="state.currentPage" @current-change="changePage" />
   </el-card>
 </template>
 
 <script setup>
 import { onMounted, reactive } from 'vue'
-import { ElMessage } from 'element-plus'
+import { ElMessage, ElMessageBox } from 'element-plus'
 import { HomeFilled, Delete } from '@element-plus/icons-vue'
 import axios from '@/utils/axios'
 
